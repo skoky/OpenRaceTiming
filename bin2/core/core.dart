e4bfbdfad07850b5c4d09393cbe0bc12d379c482
ladislav.skokan <ladislav.skokan@wincor-nixdorf.cz>
2014-10-14 20:19:19 +0200

    new dir structure

---
+++ bin2/core/core.dart
@@ -0,0 +1,32 @@
+library core;
+
+import 'package:event_commander/event_commander.dart';
+import '../device/testdevice.dart';
+import '../calculator/testcalculator.dart';
+import 'dart:isolate';
+
+EventBus event_bus;
+
+class MyEvent extends Event {
+  String selector;
+  String jsonData;
+  MyEvent(this.selector,this.jsonData);
+}
+
+class Core {
+
+Core() {
+  print("start");
+  event_bus = new EventBus();
+  
+  TestCalculator test_calculator = new TestCalculator(event_bus);
+  TestDevice test_device = new TestDevice(event_bus);
+  
+  print("stop");
+}
+
+EventBus getBus() {
+  return event_bus;
+}
+
+}
